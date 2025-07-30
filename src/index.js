b5c5538ab8a511bb6eab5ed2b5af594034410efb
husainindori786 <indorihusain786@gmail.com>
2025-07-30 12:15:40 +0530

    add cookie-parser and cors dependencies; implement ApiError, ApiResponse, and asyncHandler utilities

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: "./env"
 })
 
+const port = process.env.PORT || 8080
+
 connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`⚙️  Server is running at port : ${port}`);
+    } )
+    app.on("error", (error) => {
+        console.log("App is not connected!!",error);
+        throw error
+        
+    })
+})
+.catch((error) => {
+    console.log("MONGO DB Connection is Failed!!!!", error);
+    
+})
 
 
 
